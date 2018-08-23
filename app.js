869faa300d052e03b76fafd6073d57cc231ee802
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-08-24 03:30:34 +0700

    adding file bin www and changes script runner development environment

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const bodyPars = require('body-parser');
+const logger = require('morgan');
 const index = require('./app/routes/index');
 const cors = require('cors');
 
 const app = express();
 
+// Log requests to the console.
+app.use(logger('dev'));
+
 app.use(cors());
+
+// Parse incoming requests data
 app.use(bodyPars.json());
 app.use(bodyPars.urlencoded({extended: false}));
 
 app.use('/api', index);
 
-app.listen(3000, () => {
-  console.log('Server running');
-})
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+module.exports = app;
