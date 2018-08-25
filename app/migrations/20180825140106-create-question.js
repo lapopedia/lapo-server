c12992f065d52eb7c3cae27b5976481f28e54b8b
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-08-25 22:40:22 +0700

    new questions migrations

---
+++ app/migrations/20180825140106-create-question.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      author_id: {
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      vote: {
+        type: Sequelize.INTEGER
+      },
+      tag: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Questions');
+  }
+};
---
+++ app/models/question.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Question = sequelize.define('Question', {
+    author_id: DataTypes.INTEGER,
+    content: DataTypes.TEXT,
+    title: DataTypes.STRING,
+    vote: DataTypes.INTEGER,
+    tag: DataTypes.ARRAY(DataTypes.TEXT),
+    is_active: DataTypes.BOOLEAN
+  }, {});
+  Question.associate = function(models) {
+    // associations can be defined here
+    Question.belongsTo(models.User, { foreignKey: 'author_id', onDelete: 'CASCADE' });
+  };
+  return Question;
+};
