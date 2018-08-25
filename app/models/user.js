de06d0b6a4c650a38fd0caade61cf8fc653b013a
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-08-25 20:43:40 +0700

    new migration to adding salt and role column

--- app/models/user.js
+++ app/models/user.js
@@ -11,7 +11,9 @@ module.exports = (sequelize, DataTypes) => {
         isEmail: true
       }  
     },
-    reputation: DataTypes.INTEGER
+    reputation: DataTypes.INTEGER,
+    salt: DataTypes.STRING,
+    role: DataTypes.ENUM('UNFERIFIED_USER', 'USER', 'VERIFIED_CONTRIBUTOR', 'ADMIN')
   }, {});
   User.associate = function(models) {
     // associations can be defined here
