9cbf876dde6da6fd01bb54ab5b6bccc0bc637530
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-08-25 20:42:59 +0700

    adding get user details, delete, all users

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,20 +1,74 @@
 const model = require('../models');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const shortId = require('short-id');
+
 
 module.exports = {
   userRegister: (req, res) => {
+    const userSalt = shortId.generate();
     model.User.create({
       fullname: req.body.fullname,
       username: req.body.username,
-      password: req.body.password,
+      password: crypto.createHmac('sha256', userSalt).update(req.body.password).digest('hex'),
       email: req.body.email,
-      reputation: 100
+      reputation: 100,
+      salt: userSalt,
+      role: 'USER'
     })
     .then((user) => {
       res.send({
         status_code: 200,
         message: 'Register successfully',
         data: user
+      })
+    })
+    .catch((err) => {
+      res.send({
+        status_code: 400,
+        message: err
+      })
+    })
+  },
+  getUserDetails: (req, res) => {
+    model.User.find({
+      where: {
+        id: req.params.userId
+      }
+    }).then((user) => {
+      res.send({
+        status_code: 200,
+        data: user,
+        message: 'Get one user'
+      })
+    })
+  },
+  getAllUser: (req, res) => {
+    model.User.findAll().then((users) => {
+      if (users) {
+        res.send({
+          status_code: 200,
+          message: 'Get users successfully',
+          data: users
+        })
+      } else {
+        res.send({
+          status_code: 400,
+          message: 'Something error with your request',
+        })
+      }
+    })
+  },
+  deleteUser: (req, res) => {
+    model.User.destroy({
+      where: {
+        id: req.params.userId
+      }
+    })
+    .then((user) => {
+      res.send({
+        status_code: 200,
+        message: 'User has been deleted',
       })
     })
     .catch((err) => {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 
 router.post('/auth', UserController.userRegister);
+router.get('/users', UserController.getAllUser);
+router.get('/users/:userId', UserController.getUserDetails);
+router.delete('/users/:userId', UserController.deleteUser);
 
 module.exports = router;
